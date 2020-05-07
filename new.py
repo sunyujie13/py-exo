0709513e8b84f446ec3360b0e24d7599ceab5ee5
sunyujie13 <58180757+sunyujie13@users.noreply.github.com>
2020-05-07 17:09:32 +0200

    nex

---
+++ new.py
@@ -0,0 +1,13 @@
+print("1 - %d total: %d" % (n,sum))
+print('1- '+ str(n)+' total: '+ str(sum))
+print((n,sum))
+name = "your"
+print("www.%s.com"% name)
+print("www.%d %d.com" % (150 ,200))
+print("www.%d.com"%(150))
+langue = [ "c" , "c++" ,"PHP" ,"Python"]
+for x in langue:
+    print(x ,end = ".")
+for i in range(-10,-101,-30) :
+    print(i)
+    -30 espace
